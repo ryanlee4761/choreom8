2147c74130d6c9684981f86ee7c6a5dc26aa877c
Ryan Lee <rylee4761@berkeley.edu>
2025-08-07 15:02:35 -0700

    some cleaning up stuff

--- src/pages/Playback.jsx
+++ src/pages/Playback.jsx
@@ -1,49 +1,55 @@
 import React, { useState, useRef, useEffect } from 'react';
 
 export default function Playback() {
-    const [buttonText, setButtonText] = useState('Play'); 
+    const [playButtonText, setplayButtonText] = useState('Play');
+
     const videoRef = useRef(null);
 
     // have database folder for videos
+    const updatePlayText = () => {
+        if (videoRef.current.paused) {
+            setplayButtonText("Play");
+        } else {
+            setplayButtonText("Pause");
+        }
+    }
+    const togglePlay = () => {
+        if (videoRef.current.paused) {
+            videoRef.current.play();
+            setplayButtonText("Play");
+        } else {
+            videoRef.current.pause();
+            setplayButtonText("Pause");
+        }
+    };
 
+    // update button text when video controls are used
     useEffect(() => {
-        const handlePlay = () => {
-            setButtonText("Pause");
-        };
-        const handlePause = () => {
-            setButtonText("Play");
-        };
-
         // Add event listener to the referenced element
         if (videoRef.current) {
-            videoRef.current.addEventListener('play', handlePlay);
-            videoRef.current.addEventListener('pause', handlePause);
+            videoRef.current.addEventListener("click", updatePlayText);
+            videoRef.current.addEventListener('play', updatePlayText);
+            videoRef.current.addEventListener('pause', updatePlayText);
         }
 
         // Clean up the event listener when the component unmounts
         return () => {
             if (videoRef.current) {
-                videoRef.current.removeEventListener('play', handlePlay);
-                videoRef.current.removeEventListener('pause', handlePause);
+                videoRef.current.removeEventListener('play', updatePlayText); 
+                videoRef.current.removeEventListener('play', updatePlayText);
+                videoRef.current.removeEventListener('pause', updatePlayText);
             }
         };
     }, []);
-
-    const playPause = () => {    
-        if (videoRef.current.paused) {
-            videoRef.current.play();
-        } else {
-            videoRef.current.pause();
-            self.innerHTML = "Play";
-        }
-    }
 
     return (
         <div>
             <h1 className="text-3xl font-bold">Playback</h1>
 
             <video ref={videoRef} src="src/assets/katseye.mp4" width="200px" id="playbackvid" controls>video here</video>
-            <button type="submit" value="submit" id="playpause" onClick={playPause} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{buttonText}</button>
+            <button type="submit" value="submit" id="playpause" onClick={togglePlay} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{playButtonText}</button>
+
+            {/* <button type="submit" value="submit" id="togglestart" onClick={} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{buttonText}</button> */}
 
             <p>variable src video file? need to make some way for users to upload :/</p>
             <p>Add buttons for stuff!</p>
