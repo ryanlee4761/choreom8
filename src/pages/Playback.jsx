1c03bc1caebad9a77caf50ab087df2d99c5ecc11
Ryan Lee <rylee4761@berkeley.edu>
2025-08-06 22:31:43 -0700

    Basic video playback functionality

--- src/pages/Playback.jsx
+++ src/pages/Playback.jsx
@@ -1,3 +1,34 @@
+import React, { useState } from 'react';
+
 export default function Playback() {
-    return <h1 className="text-3xl font-bold">Playback</h1>;
+    const [buttonText, setButtonText] = useState('Play'); 
+    var video = document.getElementById("playbackvid");
+    // have database folder for videos
+    video.addEventListener("play", () => {
+        setButtonText("Pause");
+    })
+    video.addEventListener("pause", () => {
+        setButtonText("Play");
+    })
+
+    const playPause = () => {    
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+            self.innerHTML = "Play";
+        }
+    }
+
+    return (
+        <div>
+            <h1 className="text-3xl font-bold">Playback</h1>
+
+            <video src="src/assets/katseye.mp4" width="200px" id="playbackvid" controls>video here</video>
+            <button type="submit" value="submit" id="playpause" onClick={playPause} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{buttonText}</button>
+
+            <p>variable src video file? need to make some way for users to upload :/</p>
+            <p>Add buttons for stuff!</p>
+        </div>
+    );
 }
